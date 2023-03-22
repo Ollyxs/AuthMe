92d1c2e59674240855c8da5e635f41bc3b26539d
Lucas Ollarce <luk5ollarce@gmail.com>
2023-03-22 13:14:20 -0300

    close #4 configuración de redis y mysql

--- ms-otp/app.py
+++ ms-otp/app.py
@@ -6,4 +6,4 @@ app = create_app()
 app.app_context().push()
 
 if __name__ == "__main__":
-    app.run(debug = True, port = 5000)
+    app.run(debug = True, host="0.0.0.0", port = 5000)
---
+++ ms-user/test/test_app.py
@@ -0,0 +1,16 @@
+import unittest
+from flask import current_app
+from app import create_app, db
+
+
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app()
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+
+    def tearDown(self):
+        self.app_context.pop()
+
+    def test_app_exists(self):
+        self.assertIsNotNone(current_app)
