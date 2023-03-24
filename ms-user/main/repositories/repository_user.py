0fb653e9705bb0e8fa4011deec201d9e93c5d27e
Douglas <douglasarenas71@gmail.com>
2023-03-24 16:01:27 -0300

    Crud create but not finish

---
+++ ms-user/main/repositories/repository_user.py
@@ -0,0 +1,37 @@
+from .. import db
+# from main.models import ClienteModel
+from .repository_base import Create, Read, Update, Delete
+
+
+class UserRepository(Create, Read, Update, Delete):
+    def __init__(self):
+        
+        self.__model = UserModel   #Agregar nombre igual que en MODELOS
+
+
+    def find_one(self, id):
+        objeto = db.session.query(self.__model).get_or_404(id)
+        return objeto
+
+    def create(self, objeto):
+        db.session.add(objeto)
+        db.session.commit()
+        return objeto
+
+    def delete(self, id):
+        objeto = db.session.query(self.__model).get_or_404(id)
+        self.__soft_delete(objeto, id)
+
+    # ! Para que sirve esto
+    def __soft_delete(self, objeto):
+        objeto.__activado = False
+        self.update(objeto, id)
+
+    def update(self, data, id):
+        objeto = db.session.query(self.__model).get_or_404(id)
+        for key, value in data:
+            setattr(objeto, key, value)
+        db.session.add(objeto)
+        db.session.commit()
+        return objeto
+
