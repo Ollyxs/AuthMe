e3ea34be884756639dea55e6ae5438458ad33869
Douglas <douglasarenas71@gmail.com>
2023-03-24 15:17:11 -0300

    initial feature

--- ms-otp/app.py
+++
@@ -1,9 +0,0 @@
-from main import create_app
-import os
-
-
-app = create_app()
-app.app_context().push()
-
-if __name__ == "__main__":
-    app.run(debug = True, port = 5000)
--- ms-otp/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 -m venv .
-source bin/activate
-pip3 install -r requirements.txt
